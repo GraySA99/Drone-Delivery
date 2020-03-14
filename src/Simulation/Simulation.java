f29b0b1058011ae5e38f42c992dc9f6973ace082
PJKnightR <coolpj99@comcast.net>
2020-03-14 14:37:04 -0400

    Version 1.2.0

--- src/Simulation/Simulation.java
+++ src/Simulation/Simulation.java
@@ -1,5 +1,6 @@
 package Simulation;
 
+import Food.Meal;
 import Food.Order;
 
 import java.util.ArrayList;
@@ -7,9 +8,11 @@ import java.util.PriorityQueue;
 
 public class Simulation {
     ArrayList<Order> orderQueue = new ArrayList<Order>();
+    ArrayList<Meal> mealList = new ArrayList<Meal>();
     PriorityQueue<Order> currentOrderQueue = new PriorityQueue<Order>();
 
     private void startSimulation(){
+        //should add default meals (for now) to meal list
         //is what calls the run FIFO and runKnapsack methods a specified amount of times
         //calls generateOrderQueue and copy orderQueue when appropriate
     }
