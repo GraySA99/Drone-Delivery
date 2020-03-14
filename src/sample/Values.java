72b98ce35ebeae0814a0a6d7e13286110aefb94d
spencer <grayspa17@gmail.com>
2020-03-14 19:08:34 -0400

    Side Menu added Side Menu properly changes between the four main screens Side Menu has a hamburger button to get to secondary side menu Secondary Side Menu buttons are non-functional except for Quit Clicking all main screens when secondary side menu closes the secondary side menu
    Styles data class created to track CSS styles of all objects
    Values data class created to track certain properties of all objects

---
+++ src/sample/Values.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Values {
+
+    public final double sideMenuWidth = 250;
+    public final double sideMenuHeight = Double.MAX_VALUE;
+    public final double sideMenuBtnHeight = 100;
+    public final double sideMenuBtnWidth = Double.MAX_VALUE;
+
+    public final int numberOfMenues = 4;
+    public final Integer mapMenuID = 1;
+    public final Integer foodMenuID = 2;
+    public final Integer mealsMenuID = 3;
+    public final Integer shiftsMenuID = 4;
+}
