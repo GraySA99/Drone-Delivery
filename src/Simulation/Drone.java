8a674f3a5fb527099f2e7a7acd3e2c328ae622b8
PJKnightR <coolpj99@comcast.net>
2020-03-18 18:15:21 -0400

    Version 1.5.0

--- src/Simulation/Drone.java
+++ src/Simulation/Drone.java
@@ -1,14 +1,14 @@
 package Simulation;
 
 import Food.Order;
-import Mapping.Waypoint;
+//import Mapping.Waypoint;
 
 import java.util.ArrayList;
 
 public class Drone {
     private ArrayList<Order> loadedOrdersList = new ArrayList<Order>();
     private double weightCapacity;
-    private Waypoint currentPosition, targetPosition;
+    //private Waypoint currentPosition, targetPosition;
 
     public Order getOrderOnDrone(int i){
         return loadedOrdersList.get(i);
@@ -18,12 +18,12 @@ public class Drone {
         return weightCapacity;
     }
 
-    public Waypoint getCurrentPosition(){
-        return currentPosition;
-    }
+    //public Waypoint getCurrentPosition(){
+        //return currentPosition;
+    //}
 
-    public Waypoint getTargetPosition(){
-        return targetPosition;
-    }
+    //public Waypoint getTargetPosition(){
+        //return targetPosition;
+    //}
 
 }
--- src/Simulation/Simulation.java
+++ src/Simulation/Simulation.java
@@ -9,9 +9,9 @@ import java.util.PriorityQueue;
 public class Simulation {
     private ArrayList<Order> orderQueue = new ArrayList<Order>();
     private ArrayList<Meal> mealList = new ArrayList<Meal>(); //stores all different possible meals. Will be passed on constructor
-    private PriorityQueue<Order> currentOrderQueue = new PriorityQueue<Order>();
+    private ArrayList<Order> currentOrderQueue = new ArrayList<Order>();
     private int numShifts, timesToBeRan; //passed in constructor, stores number of hours to do the simulation and number of dif sims
-    private int[] avgOrders = new int[numShifts];
+    private int[] ordersPerHour, times;
 
     //Create array for delivery times with the sum of the number of orders in all of the shifts.
     public float [] deliveryTimes = new float[60];
@@ -23,11 +23,14 @@ public class Simulation {
     //the check for adding the probabilities needs to be somewhere. Should be outside of this class
     public Simulation(){
         numShifts = 4;
+        ordersPerHour = new int[numShifts];
         timesToBeRan = 1;
-        avgOrders[0] = 15;
-        avgOrders[1] = 17;
-        avgOrders[2] = 22;
-        avgOrders[3] = 15;
+        ordersPerHour[0] = 15;
+        ordersPerHour[1] = 17;
+        ordersPerHour[2] = 22;
+        ordersPerHour[3] = 15;
+
+        times = new int[ordersPerHour[0] + ordersPerHour[1] + ordersPerHour[2] + ordersPerHour[3]];
 
         ArrayList<Food> temp = new ArrayList<Food>();
         temp.add(new Hamburger());
@@ -40,7 +43,7 @@ public class Simulation {
         mealList.add(new Meal(temp, 0.1));
 
         temp.remove(2);
-        temp.remove(3);
+        temp.remove(2);
         mealList.add(new Meal(temp, 0.2));
 
         temp.add(new Hamburger());
@@ -51,14 +54,39 @@ public class Simulation {
         //this will be used when we start having settings for the simulation
     }
 
-    private void runSimulation(){
+    public void runSimulation(){
         //should add default meals (for now) to meal list
         //is what calls the run FIFO and runKnapsack methods a specified amount of times
         //calls generateOrderQueue and copy orderQueue when appropriate
+
+        for(int i = 0; i < timesToBeRan; i++){
+            generateOrderQueue();
+            //copyOrderQueue();
+
+            //runFIFO();
+
+            //copyOrderQueue();
+
+            //runKnapsack();
+        }
     }
 
     private void generateOrderQueue(){
-        //generates a new order queue at the beginning of each pair of simulations
+        int count = 0;
+
+        for(int r = orderQueue.size(); r > 0; r--){
+            orderQueue.remove(r - 1);
+        }
+
+        for(int i = 0; i < numShifts; i++){
+            for(int j = 0; j < ordersPerHour[i]; j++){
+                //orderQueue.add(new Order(mealList.get((int)(Math.random() * ((mealList.size() - 1) + 1))), )); //need a way to get a list of destinations
+                times[count] = (i * 60) + (int) (1 + Math.random() * ((60 - 1) + 1));
+                count++;
+            }
+        }
+
+        heapSort(times);
     }
 
     private void copyOrderQueue(){
@@ -67,13 +95,66 @@ public class Simulation {
 
     private void runFIFO(){
         //runs FIFO simulations
+        //at start, wait for 5 min or until full
+        //go minute by minute. Update the current order queue with any new orders, see where the drone is. if its at the sac pick up orders.
+        //if not then let it keep doing its thing. Maybe store the time for the drones current route
+        int currentTime = 0;
+
+        while (currentTime < numShifts * 60){
+            //fifo
+        }
     }
 
     private void runKnapsack(){
         //runs knapsack simulations
+        //at start, wait for 5 min or until full
+        //go minute by minute. Update the current order queue with any new orders, see where the drone is. if its at the sac pick up orders.
+        //if not then let it keep doing its thing
+        int currentTime = 0;
+
+        while (currentTime < numShifts * 60){
+            //fifo
+        }
     }
 
     private void getNextRoute(){
         //will use the drones current location and current orders to solve the traveling salesman problem
+    }
+
+    public void heapSort(int a[])
+    {
+        int size = a.length;
+
+        for (int j = size/2 - 1; j >= 0; j--) {
+            createHeap(a, j, size);
+        }
+        int temp;
+
+        for (int i = size - 1; i >= 0; i--) {
+            temp = a[0];
+            a[0] = a[i];
+            a[i] = temp;
+
+            createHeap(a, 0, i);
+        }
+
+    }
+
+    void createHeap(int a[], int root, int size)
+    {
+        int largest = root, left = 2 * root + 1, right = 2 * root + 2;
+
+        if (left < size && a[left] > a[largest]){
+            largest = left;
+        }
+        if (right < size && a[right] > a[largest]){
+            largest = right;
+        }
+        if (largest != root){
+            int temp = a[root];
+            a[root] = a[largest];
+            a[largest] = temp;
+            createHeap(a, largest, size);
+        }
     }
 }
