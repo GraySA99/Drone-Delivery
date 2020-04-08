91f153ae76feded117953e8430581c646b2649d2
Josh Worley <45806673+joshworley@users.noreply.github.com>
2020-04-08 11:51:51 -0400

    Merge remote-tracking branch 'origin/master' into Josh
    # Conflicts:
    #	src/Mapping/Waypoint.java
    #	src/Simulation/Simulation.java

--- src/sample/SideMenu.java
+++ src/sample/SideMenu.java
@@ -158,7 +158,7 @@ public class SideMenu extends ToolBar {
                     FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
                     fileChooser.getExtensionFilters().add(filter);
 
-                    //Show save file dialogue // NEED TO MAKE SURE I HAVE THE RIGHT STAGE!
+                    //Show save file dialogue
                     File file = fileChooser.showSaveDialog(Values.primaryStage);
 
                     if(file != null) {
@@ -202,6 +202,7 @@ public class SideMenu extends ToolBar {
 
     private String getResultsStr() {
         String ret = "";
+
 
         return ret;
     }
